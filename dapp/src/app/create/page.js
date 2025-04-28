8963c528ebd284e17d0824a5596c4b1e7595258f
Felipe Possari <fcpfelipe@hotmail.com>
2025-04-28 10:26:28 -0300

    Add campaing function created

--- dapp/src/app/create/page.js
+++ dapp/src/app/create/page.js
@@ -1,14 +1,31 @@
 "use client";
 
 import { useState } from "react";
+import { addCampaing, nextId } from "@/services/web3Service";
 
 export default function Create() {
   const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [campaign, setCampaign] = useState({});
 
-  function btnLoginClick() {
-    //push('/create');
-    setMessage('Campaign created!');
+  function onInputChange(event) {
+    setCampaign({ ...campaign, [event.target.id]: event.target.value });
+  }
+
+  function btnSaveClick() {
+    setMessage('');
+    setErrorMessage('');
+    setMessage('Saving campaing');
+    addCampaing(campaign)
+      .then(() => nextId())
+      .then(id => {
+        setMessage(`Campaign created! Please use this link to donate: http://localhost:3000/donate/${id}`);
+      })
+      .catch(err => {
+        setMessage('');
+        console.error(err);
+        setErrorMessage(err.message);
+      });
   }
 
   return (
@@ -19,23 +36,27 @@ export default function Create() {
         <hr className="mb-4" />
         <div className="col-6">
           <div className="form-floating mb-3">
-            <input type="text" id="title" className="form-control" placeholder="Title" />
+            <input type="text" id="title" className="form-control" placeholder="Title"
+              onChange={onInputChange} value={campaign.title || ""} />
             <label htmlFor="title">Title</label>
           </div>
           <div className="form-floating mb-3">
-            <textarea id="description" className="form-control" placeholder="Description" />
+            <textarea id="description" className="form-control" placeholder="Description"
+              onChange={onInputChange} value={campaign.description || ""} />
             <label htmlFor="description">Description</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="text" id="imageUrl" className="form-control" placeholder="Link" />
+            <input type="text" id="imageUrl" className="form-control" placeholder="Link"
+              onChange={onInputChange} value={campaign.imageUrl || ""} />
             <label htmlFor="link">Image URL</label>
           </div>
           <div className="form-floating mb-3">
-            <input type="text" id="videoUrl" className="form-control" placeholder="Image" />
+            <input type="text" id="videoUrl" className="form-control" placeholder="Image"
+              onChange={onInputChange} value={campaign.videoUrl || ""} />
             <label htmlFor="image">Video URL</label>
           </div>
           <div className="col-12 mb-3">
-            <button className="btn btn-primary col-12 p-3" onClick={btnLoginClick}>Create</button>
+            <button className="btn btn-primary col-12 p-3" onClick={btnSaveClick}>Create</button>
           </div>
           {
             message ?
@@ -43,10 +64,10 @@ export default function Create() {
               <></>
           }
           {
-              errorMessage ?
-                <div className="alert alert-danger p-3 col-12 mt-3" role="alert">{errorMessage}</div> :
-                <></>
-            }
+            errorMessage ?
+              <div className="alert alert-danger p-3 col-12 mt-3" role="alert">{errorMessage}</div> :
+              <></>
+          }
         </div>
 
       </div>
--- dapp/src/services/web3Service.js
+++ dapp/src/services/web3Service.js
@@ -1,13 +1,36 @@
 import Web3 from 'web3';
+import ABI from './ABI.json';
 
-export async function login(){
-    if(!window.ethereum) throw new Error('No metamask detected');
+const CONTRACT_ADDRESS = '0x74bb682Cb000E1f46931A9efAf184101Ba17bE36';
+
+export async function login() {
+    if (!window.ethereum) throw new Error('No metamask detected');
 
     const web3 = new Web3(window.ethereum);
     const accounts = await web3.eth.requestAccounts();
 
-    if(!accounts || !accounts.length) throw new Error('No accounts detected');
+    if (!accounts || !accounts.length) throw new Error('No accounts detected');
 
     localStorage.setItem('wallet', accounts[0]);
     return accounts[0];
+}
+
+function getContract() {
+    const web3 = new Web3(window.ethereum);
+    const from = localStorage.getItem('wallet');
+    return new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from });
+}
+
+export async function addCampaing(campaign) {
+    const contract = getContract();
+    return contract.methods
+    .addCampaing(campaign.title, 
+        campaign.description, 
+        campaign.videoUrl, 
+        campaign.imageUrl).send();
+}
+
+export async function nextId() {
+    const contract = getContract();
+    return contract.methods.nextId().call();
 }
